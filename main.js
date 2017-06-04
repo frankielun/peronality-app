1cfafa20a5f6d683b7abb4ca762eac45d5a271b9
FrankieLun <frankielun@gmail.com>
2017-06-04 18:16:44 +0800

    added eslint config to support react-native

--- main.js
+++ main.js
@@ -2,23 +2,21 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
+const bgColor = '#fff';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: bgColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const App = () =>
+(
+  <View style={styles.container}>
+    <Text>Open up main.js to start working on your app!</Text>
+  </View>
+);
 
 Expo.registerRootComponent(App);
