1c04115a0a6cf704a6e33c0350d6359e69748ce4
FrankieLun <frankielun@gmail.com>
2017-06-05 23:48:57 +0800

    Setup redux basic

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import Expo from 'expo';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import TabRouter from './src/router/TabRouter';
+import rootReducer from './src/reducers/RootReducer';
+
+const store = createStore(rootReducer);
 
 const App = () =>
 (
-  <TabRouter />
+  <Provider store={store}>
+    <TabRouter />
+  </Provider>
 );
 
 Expo.registerRootComponent(App);
---
+++ src/reducers/RootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import TestingReducer from './TestingReducer';
+
+const RootReducers = combineReducers({
+  TestingReducer,
+});
+
+export default RootReducers;
